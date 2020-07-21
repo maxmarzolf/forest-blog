e7638465591ed5710ec864eaccdc04b0d5cfe25b
maxmarzolf <max@treesfromspace.com>
2020-07-21 18:38:17 -0400

    pages

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/',  methods=['GET'])
 def show_homepage():
-    return render_template('home.html')
+    return render_template('pages/home.html')
 
 
 if __name__ == '__main__':
