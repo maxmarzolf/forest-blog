1e12d23484dcd1dcd484d7029f8d7cb0480434ab
maxmarzolf <max@treesfromspace.com>
2020-09-26 15:23:50 -0400

    my first pytest, validating ok response, hooray!

---
+++ test_1.py
@@ -0,0 +1,17 @@
+import pytest
+from app import app
+
+
+def test_assert():
+    assert True
+
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
